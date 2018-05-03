58e47cabff5b53807a48dc188fe938a513e695d6
TeeanRonson <31168019+TeeanRonson@users.noreply.github.com>
2018-05-02 17:22:24 -0700

    "Incomplete"

--- src/graph/Dijkstra.java
+++ src/graph/Dijkstra.java
@@ -5,21 +5,30 @@ package graph;
  *  Fill in code. You may add additional helper methods or classes.
  */
 
+import com.sun.tools.corba.se.idl.InterfaceGen;
+
 import java.util.*;
 import java.awt.Point;
 
 public class Dijkstra {
 	private Graph graph; // stores the graph of CityNode-s and edges connecting them
     private List<Integer> shortestPath = null; // nodes that are part of the shortest path
+	boolean[] known;
+	int[] distances;
+	int[] previous;
 
-    /** Constructor
+	/** Constructor
 	 *
 	 * @param filename name of the file that contains info about nodes and edges
      * @param graph graph
 	 */
 	public Dijkstra(String filename, Graph graph) {
-	    this.graph = graph;
+		this.graph = graph;
+
 		graph.loadGraph(filename);
+		this.known = new boolean[graph.numNodes()];
+		this.distances = new int[graph.numNodes()];
+		this.previous = new int[graph.numNodes()];
 	}
 
 	/**
@@ -32,19 +41,113 @@ public class Dijkstra {
 	 */
 	public List<Integer> computeShortestPath(CityNode origin, CityNode destination) {
 
-	    // FILL IN CODE
+		PriorityQueue queue = new PriorityQueue();
+		Edge[] adjacencyList = graph.getAdjacencyList();
 
-        // Create and initialize Dijkstra's table
-        // Create and initialize a Priority Queue
+		initializeTable();
+		int origId = graph.getId(origin);
+		setOrigin(known, distances, previous, origId); // Set origin information in table
 
-        // Run Dijkstra
+		Nodes currNode = new Nodes(origId, 0);
+		queue.insert(currNode.getId(), currNode.getPriority());
 
-        // Compute the nodes on the shortest path by "backtracking" using the table
 
-        // The result should be in an instance variable called "shortestPath" and
-        // should also be returned by the method
-	    return null; // don't forget to change it
-    }
+		while (queue.hasElement()) {
+			currNode = queue.removeMin();
+			this.known[currNode.getId()] = true;
+			Edge currEdge = adjacencyList[currNode.getId()];
+//			System.out.println("At nodeId: " + currNode.getId());
+			int currDist = currNode.getPriority(); // grab the distance from the table?? Instead
+			while (currEdge != null) {
+//				System.out.println("Current edge: " + currEdge.getNeighbor() + " " + currEdge.getCost());
+				int neighbourId = currEdge.getNeighbor();
+
+				if(known[neighbourId] == true) {
+					currEdge = currEdge.getNext();
+				} else {
+					//Update distances table
+					int newDistance = currDist + currEdge.getCost();
+					if (newDistance < distances[neighbourId]) {
+						distances[currEdge.getNeighbor()] = newDistance;
+						//Update previous table
+						previous[neighbourId] = currNode.getId();
+					}
+					//Insert elements into the queue
+					queue.insert(neighbourId, currDist + currEdge.getCost());
+					//Update Edge
+					currEdge = currEdge.getNext();
+				}
+			}
+			System.out.println();
+		}
+
+		for (int i = 0; i < known.length; i++) {
+			System.out.print(i + " ");
+			System.out.print(known[i] + " ");
+		}
+		System.out.println();
+
+		for (int i = 0; i < distances.length; i++) {
+			System.out.print(distances[i] + " ");
+		}
+		System.out.println();
+
+		for (int i = 0; i < previous.length; i++) {
+			System.out.print(previous[i] + " ");
+		}
+		System.out.println();
+
+		return getShortestPath(origin, destination);
+	}
+
+	private List<Integer> getShortestPath(CityNode origin, CityNode destination) {
+
+		shortestPath = new ArrayList<>();
+		int destId = graph.getId(destination);
+
+		while(destId != graph.getId(origin)) {
+			shortestPath.add(0, destId);
+			destId = this.previous[destId];
+		}
+		shortestPath.add(0, graph.getId((origin)));
+//		while(destId != -1 && i < graph.numNodes()) {
+//			int prevId = this.previous[destId];
+//			System.out.println("Previous id: " + prevId);
+//			temp[i] = prevId;
+//			destId = this.previous[prevId];
+//			i++;
+////			System.out.println("This is negative one: " + destId);
+//		}
+//
+//		while (i-1 >= 0) {
+////			System.out.println("here: " + i);
+//			System.out.println("Path: " + temp[i-1]);
+//			result.add(temp[i-1]);
+//			i--;
+//		}
+
+
+		return shortestPath;
+	}
+
+
+    private void initializeTable() {
+
+		for (int i = 0; i < distances.length; i++) {
+			known[i] = false;
+			distances[i] = Integer.MAX_VALUE;
+			previous[i] = -1;
+		}
+	}
+
+    private void setOrigin(boolean[] known, int[] distances, int[] previous, int original) {
+
+		known[original] = true;
+		distances[original] = 0;
+		previous[original] = -1;
+
+	}
+
 
     /**
      * Return the shortest path as a 2D array of Points.
